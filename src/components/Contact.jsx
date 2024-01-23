3ab6840d97468d78f319e2a1d1196088cc7e8c72
Zeb <moffat.zeb@gmail.com>
2024-01-23 15:52:33 -0700

    updated contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,24 +4,34 @@ function Contact() {
       <br />
       <br />
       <p>
-        &nbsp;&nbsp;&nbsp;&nbsp;Email:{" "}
+        &nbsp;&nbsp;&nbsp;&nbsp;Email:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <a href="mailto:moffat.zeb@gmail.com">moffat.zeb@gmail.com</a>
       </p>
       <br />
       <p>
-        &nbsp;&nbsp;&nbsp;&nbsp;GitHub:{" "}
+        &nbsp;&nbsp;&nbsp;&nbsp;GitHub:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <a href="https://github.com/zebmoffat" target="_blank">
           https://github.com/zebmoffat
         </a>
       </p>
       <br />
       <p>
-        &nbsp;&nbsp;&nbsp;&nbsp;LinkedIn:{" "}
+        &nbsp;&nbsp;&nbsp;&nbsp;LinkedIn:&nbsp;&nbsp;&nbsp;&nbsp;
         <a
           href="https://www.linkedin.com/in/zeb-moffat-a52229279/"
           target="_blank"
         >
           https://www.linkedin.com/in/zeb-moffat-a52229279/
+        </a>
+      </p>
+      <br />
+      <p>
+        &nbsp;&nbsp;&nbsp;&nbsp;Handshake:&nbsp;&nbsp;&nbsp;
+        <a
+          href="https://app.joinhandshake.com/stu/users/30884989"
+          target="_blank"
+        >
+          https://app.joinhandshake.com/stu/users/30884989
         </a>
       </p>
       <br />
