477446cccc300230a0878a5f6ef0bf70c92c84f3
Zeb <moffat.zeb@gmail.com>
2024-01-23 20:59:25 -0700

    pdf removed

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,11 +4,7 @@ function Resume() {
       <br />
       <br />
       <p>
-        &nbsp;&nbsp;&nbsp;&nbsp;My resume can be downloaded as a PDF&nbsp;
-        <em>
-          <a href="Zeb_Moffat_Resume.pdf" download="Zeb_Moffat_Resume.pdf">here</a>
-        </em>
-        . Or it can be found online&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;My resume can be found online&nbsp;
         <em>
           <a href="https://docs.google.com/document/d/1NDXxVKibV6HrRfRyC9rD9j5goF9Da_PM0RK2qj8KR1g/edit?usp=sharing"
             target="_blank">here</a>
